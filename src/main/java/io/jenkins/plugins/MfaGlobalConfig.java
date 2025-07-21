5cb52d68f8fcc7da201fe2f95669090b9d3d8ea3
Allan Barcelos <allan@barcelos.dev>
2025-07-21 09:08:10 -0400

    fixed - The class should override getCategory() so that the config is shown as part of the security configuration page -

--- src/main/java/io/jenkins/plugins/MfaGlobalConfig.java
+++ src/main/java/io/jenkins/plugins/MfaGlobalConfig.java
@@ -3,15 +3,20 @@
  *
  * Class: MfaGlobalConfig
  *
- *
+ * --
  * Author: Allan Barcelos
  * Date: 2025-07-18
+ *
+ * Author: Allan Barcelos
+ * Date: 2025-07-21
+ * The class should override getCategory() so that the config is shown as part of the security configuration page
  */
 
 package io.jenkins.plugins;
 
 import hudson.Extension;
 import jenkins.model.GlobalConfiguration;
+import jenkins.model.GlobalConfigurationCategory;
 import org.kohsuke.stapler.DataBoundSetter;
 
 @Extension
@@ -26,6 +31,11 @@ public class MfaGlobalConfig extends GlobalConfiguration {
 
     public MfaGlobalConfig() {
         load();
+    }
+
+    @Override
+    public GlobalConfigurationCategory getCategory() {
+        return GlobalConfigurationCategory.get(GlobalConfigurationCategory.Security.class);
     }
 
     public boolean isEnforceMfaForAllUsers() {
